07837febfc65bf0aed207396c6ad515ef17b5882
elmerlusa <martingallegojavier@gmail.com>
2025-04-23 15:08:35 +0200

    react app con typescript, materialui, materialicons y react-router

---
+++ src/frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { Icon } from '@mui/material';
+
+const App = () => {
+  return (
+    <div className="App">
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+      <header className="App-header">
+      </header>
+      <Icon>star</Icon>
+    </div>
+  );
+}
+
+export default App;
